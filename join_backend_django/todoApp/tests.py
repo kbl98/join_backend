0382483c0c0a9595ccee65d5659f6d6c0b51a3c4
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-06-06 10:04:29 +0200

    tests

--- join_backend_django/todoApp/tests.py
+++ join_backend_django/todoApp/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+import unittest
+from django.http import HttpResponse
+import json
+
 
 # Create your tests here.
+class LoginTest(TestCase):
+    def test_login(self):
+        self.client=Client()
+        self.user=User.objects.create(username='test_user',password='test_user')
+        self.client.login(username='test_user',password='test_user')
+        self.client.get('/tasks/')
+        response=HttpResponse()
+        self.assertEqual(response.status_code, 200)
+
+class PostTask(TestCase):
+    def test_postTask(self):
+        self.client = Client()
+        self.user=User.objects.create(username='test_user',password='test_user')
+        self.client.login(username='test_user',password='test_user')
+        self.obj={'title':'testtitle','description':'testdescription','prio':'medium','category':'testcat','progress':'todo','contactNames':'[{"name":"test_user"}]','subtasks':'[]'}
+       
+        data=self.obj
+        self.client.post('/tasks/',data)
+        response=HttpResponse()
+        self.assertEqual(response.status_code, 200)
+
+
+        
+    
